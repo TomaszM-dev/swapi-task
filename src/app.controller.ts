06690fabbfa813f6868f6ac0959e061328033b85
TomaszM-dev <tomekmalocha@gmail.com>
2023-12-16 19:40:25 +0100

    creating endpoints & fetching data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,53 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('films')
+  async getFilms() {
+    try {
+      const response = await this.appService.getFilms();
+      return response.data.results;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Get('species')
+  async getSpecies() {
+    try {
+      const response = await this.appService.getSpecies();
+      return response.data.results;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Get('vehicles')
+  async getVehicles() {
+    try {
+      const response = await this.appService.getVehicles();
+      return response.data.results;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Get('starships')
+  async getStarships() {
+    try {
+      const response = await this.appService.getStarships();
+      return response.data.results;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Get('planets')
+  async getPlanets() {
+    try {
+      const response = await this.appService.getPlanets();
+      return response.data.results;
+    } catch (error) {
+      throw error;
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosResponse } from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getFilms(): Promise<AxiosResponse<any>> {
+    return axios.get(`${process.env.API_URL}/films/`);
+  }
+
+  async getSpecies(): Promise<AxiosResponse<any>> {
+    return axios.get(`${process.env.API_URL}/species/`);
+  }
+
+  async getVehicles(): Promise<AxiosResponse<any>> {
+    return axios.get(`${process.env.API_URL}/vehicles/`);
+  }
+
+  async getStarships(): Promise<AxiosResponse<any>> {
+    return axios.get(`${process.env.API_URL}/starships/`);
+  }
+
+  async getPlanets(): Promise<AxiosResponse<any>> {
+    return axios.get(`${process.env.API_URL}/planets/`);
   }
 }
