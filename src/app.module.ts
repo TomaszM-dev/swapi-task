b13828d4a698620cd76281484d94d04c7f742444
TomaszM-dev <tomekmalocha@gmail.com>
2023-12-17 22:18:23 +0100

    setting up docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { CachedResource } from '../cached-resource.entity';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+type TypeORMDbType = 'sqlite';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: './data/database.db',
+      type: (process.env.DB_TYPE as TypeORMDbType) || 'sqlite',
+      database: process.env.DB_DATABASE || './data/database.db',
       entities: [CachedResource],
-      synchronize: true,
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
     }),
     TypeOrmModule.forFeature([CachedResource]),
   ],
